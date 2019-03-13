9ec194ae7f630363ffac785e44499d9a049f09cd
vmotygin <48514632+vmotygin@users.noreply.github.com>
2019-03-13 19:21:10 +0300

    Это же bash, тут не нужны &&

--- dp-local/pull-images.sh
+++ dp-local/pull-images.sh
@@ -2,8 +2,8 @@
 
 docker login \
 #&& docker-compose build \
-&& docker commit dp-nginx deliverypartsru/dp-nginx && docker push deliverypartsru/dp-nginx \
-&& docker commit dp-php deliverypartsru/dp-php && docker push deliverypartsru/dp-php \
-&& docker commit dp-worker deliverypartsru/dp-worker && docker push deliverypartsru/dp-worker \
-&& docker commit dp-traefik deliverypartsru/dp-traefik && docker push deliverypartsru/dp-traefik \
-%% echo "Images has been pushed on hub"
+docker commit dp-nginx deliverypartsru/dp-nginx && docker push deliverypartsru/dp-nginx
+docker commit dp-php deliverypartsru/dp-php && docker push deliverypartsru/dp-php
+docker commit dp-worker deliverypartsru/dp-worker && docker push deliverypartsru/dp-worker
+docker commit dp-traefik deliverypartsru/dp-traefik && docker push deliverypartsru/dp-traefik
+echo "Images has been pushed on hub"
