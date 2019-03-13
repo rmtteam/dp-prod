6d04154dd099e82d84c349fdb0f9f9869ad38671
vmotygin <48514632+vmotygin@users.noreply.github.com>
2019-03-13 19:17:46 +0300

    ТОпорный скрипт деплоя

---
+++ dp-local/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker login \
+&& cd dp-local/ \
+#&& docker-compose build \
+&& docker commit dp-nginx deliverypartsru/dp-nginx && docker push deliverypartsru/dp-nginx \
+&& docker commit dp-php deliverypartsru/dp-php && docker push deliverypartsru/dp-php \
+&& docker commit dp-worker deliverypartsru/dp-worker && docker push deliverypartsru/dp-worker \
+&& docker commit dp-traefik deliverypartsru/dp-traefik && docker push deliverypartsru/dp-traefik \
+%% echo "Images has been pushed on hub"
