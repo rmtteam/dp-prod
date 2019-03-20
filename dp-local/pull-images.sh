30690646369581b98512d85b343d3b20be683467
LHAV <lhav@MacBook-Pro-LHAV.local>
2019-03-20 22:55:28 +0700

    Добавил версию image в pull скрипт

--- dp-local/pull-images.sh
+++ dp-local/pull-images.sh
@@ -2,8 +2,8 @@
 
 docker login
 docker-compose build
-docker commit dp-nginx deliverypartsru/dp-nginx && docker push deliverypartsru/dp-nginx
-docker commit dp-php deliverypartsru/dp-php && docker push deliverypartsru/dp-php
-docker commit dp-worker deliverypartsru/dp-worker && docker push deliverypartsru/dp-worker
-docker commit dp-traefik deliverypartsru/dp-traefik && docker push deliverypartsru/dp-traefik
+docker commit dp-nginx deliverypartsru/dp-nginx:1.3.20 && docker push deliverypartsru/dp-nginx:1.3.20
+docker commit dp-php deliverypartsru/dp-php:1.3.20 && docker push deliverypartsru/dp-php:1.3.20
+docker commit dp-worker deliverypartsru/dp-worker:1.3.20 && docker push deliverypartsru/dp-worker:1.3.20
+docker commit dp-traefik deliverypartsru/dp-traefik:1.3.20 && docker push deliverypartsru/dp-traefik:1.3.20
 echo "Images has been pushed on hub"
