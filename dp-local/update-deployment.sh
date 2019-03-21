a8c47b2115cf15598fdbd158fb7c61ebccbd0d45
LHAV <lhav@MacBook-Pro-LHAV.local>
2019-03-22 02:54:09 +0700

    Скрипт обновления deployment

---
+++ dp-local/update-deployment.sh
@@ -0,0 +1,3 @@
+kubectl replace -f dp-kuber/deployment/dp-worker-deployment.yaml --force
+kubectl replace -f dp-kuber/deployment/dp-nginx-deployment.yaml --force
+kubectl replace -f dp-kuber/deployment/dp-php-deployment.yaml --force
