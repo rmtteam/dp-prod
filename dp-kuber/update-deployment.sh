2b0b509cb6c6e121ae01e0d8c8dbe47e745ab409
LHAV <lhav@MacBook-Pro-LHAV.local>
2019-03-22 03:15:14 +0700

    Перенос скрипта

---
+++ dp-kuber/update-deployment.sh
@@ -0,0 +1,3 @@
+kubectl replace -f deployment/dp-worker-deployment.yaml --force
+kubectl replace -f deployment/dp-nginx-deployment.yaml --force
+kubectl replace -f deployment/dp-php-deployment.yaml --force
--- dp-local/update-deployment.sh
+++
@@ -1,3 +0,0 @@
-kubectl replace -f dp-kuber/deployment/dp-worker-deployment.yaml --force
-kubectl replace -f dp-kuber/deployment/dp-nginx-deployment.yaml --force
-kubectl replace -f dp-kuber/deployment/dp-php-deployment.yaml --force
