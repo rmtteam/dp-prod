547b804bc8fc08f86f0317eb3abf7333e2b521b0
LHAV <lhav@MacBook-Pro-LHAV.local>
2019-03-22 02:49:54 +0700

    1.3.22.1

--- dp-local/pull-images.sh
+++ dp-local/pull-images.sh
@@ -12,8 +12,8 @@ echo "All removed"
 
 docker login
 docker-compose up --build --no-start
-docker commit dp-nginx deliverypartsru/dp-nginx:1.3.21.1 && docker push deliverypartsru/dp-nginx:1.3.21.1
-docker commit dp-php deliverypartsru/dp-php:1.3.21.1 && docker push deliverypartsru/dp-php:1.3.21.1
-docker commit dp-worker deliverypartsru/dp-worker:1.3.21.1 && docker push deliverypartsru/dp-worker:1.3.21.1
-docker commit dp-traefik deliverypartsru/dp-traefik:1.3.21.1 && docker push deliverypartsru/dp-traefik:1.3.21.1
+docker commit dp-nginx deliverypartsru/dp-nginx:1.3.22.1 && docker push deliverypartsru/dp-nginx:1.3.22.1
+docker commit dp-php deliverypartsru/dp-php:1.3.22.1 && docker push deliverypartsru/dp-php:1.3.22.1
+docker commit dp-worker deliverypartsru/dp-worker:1.3.22.1 && docker push deliverypartsru/dp-worker:1.3.22.1
+docker commit dp-traefik deliverypartsru/dp-traefik:1.3.22.1 && docker push deliverypartsru/dp-traefik:1.3.22.1
 echo "Images has been pushed on hub"
